0324afa0104b3d256502fee9cabde541067f67ce
nataoleynikova <olejnikova-n@yandex.ru>
2024-01-18 20:19:35 +0300

    changed coffee cup

--- coffee cup/index.js
+++ coffee cup/index.js
@@ -1,5 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
 	
+	let button = document.querySelector(".play-button");
+	button.addEventListener('click', () => {
+		anime({
+			targets: document.querySelectorAll(".dot"),
+		
+			loop: false,
+			easing: "linear",
+			opacity: [
+				{ value: 1, duration: 100, delay: anime.stagger(2) },
+				{ value: 0, duration: function(){return anime.random(500,1500);}}
+			],
+			width: { 
+				value: 2, duration: 500, delay: anime.stagger(2) 
+			},
+			height: { 
+				value: 2, duration: 500, delay: anime.stagger(2) 
+			},
+			
+			translateX: {
+				value: function() {
+					return anime.random(-30, 30);
+				},
+				duration: 4000,
+				delay: anime.stagger(2)
+			},
+	
+			translateY: {
+				value: function() {
+					return anime.random(-30, 30);
+				},
+				duration: 4000,
+				delay: anime.stagger(2)
+			},
+		});
+	})
+	
 	let coffee = document.querySelector(".coffee");
 	let foam = document.querySelector('.foam');
 	
@@ -25,43 +61,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		
 			dot.style.left = x + anime.random(50, 50) + "%";
 			dot.style.top = y + anime.random(50, 50) + "%";
-			
-			dot.style.opacity = "0";
 		};
 	};
-	
-	anime({
-		targets: document.querySelectorAll(".dot"),
-	
-		loop: true,
-		easing: "linear",
-		opacity: [
-			{ value: 1, duration: 100, delay: anime.stagger(2) },
-			{ value: 0, duration: function(){return anime.random(500,1500);}}
-		],
-		width: { 
-			value: 2, duration: 500, delay: anime.stagger(2) 
-		},
-		height: { 
-			value: 2, duration: 500, delay: anime.stagger(2) 
-		},
-		
-		translateX: {
-			value: function() {
-				return anime.random(-30, 30);
-			},
-			duration: 4000,
-			delay: anime.stagger(2)
-		},
-
-		translateY: {
-			value: function() {
-				return anime.random(-30, 30);
-			},
-			duration: 4000,
-			delay: anime.stagger(2)
-		},
-
-		// autoplay: false
-	});
 })
